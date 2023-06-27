bfe0a7db80ba55bfdaf93cd2d46a00877bc44a59
MaximeRtr <maxchoupinou@hotmail.fr>
2023-06-27 18:53:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $BDD_host="localhost";
 $BDD_user="maxime";
 $BDD_password="123"; 
-$BDD_base="grp4_circuit_training"; // nom de la base de données
+$BDD_base="projet"; // nom de la base de données
 ?>
