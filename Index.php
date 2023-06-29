411b5b8e63f1fd0101d18fe56ba6c898fb1d239e
Shandinark <137493014+Shandinark@users.noreply.github.com>
2023-06-29 23:01:31 +0200

    Arrangement voirseance

--- Index.php
+++ Index.php
@@ -1,9 +1,15 @@
 <?php
 session_start();
 if (isset($_SESSION["idUser"])){
+<<<<<<< Updated upstream
 $id=$_SESSION["idUser"];
 echo ("<script>idUser={$id};
 console.log(idUser);</script>");
+=======
+    $id=$_SESSION["idUser"];
+    echo ("<script>idUser={$id};
+    console.log(idUser);</script>");
+>>>>>>> Stashed changes
 }
 /*
 Cette page génère les différentes vues de l'application en utilisant des templates situés dans le répertoire "templates". Un template ou 'gabarit' est un fichier php qui génère une partie de la structure XHTML d'une page. 
