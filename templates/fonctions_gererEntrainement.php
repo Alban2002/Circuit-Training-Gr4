7045524d3d527ffebfc3be22c266b9a49f6fca11
Shandinark <137493014+Shandinark@users.noreply.github.com>
2023-06-30 01:54:19 +0200

    updt fonction_gererEnctrainement

--- templates/fonctions_gererEntrainement.php
+++ templates/fonctions_gererEntrainement.php
@@ -14,40 +14,7 @@ class SeanceManager
     {
         $this->db = $db;
     }
-    public function fetchUserSeances($userId)
-    {
-        $query = "
-            SELECT as1.ID_seance, as1.date 
-            FROM attribution_seance as1
-            WHERE as1.ID_user = :userId
-            ORDER BY as1.date ASC
-        ";
 
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
-        $stmt->execute();
-
-        $seances = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        return $seances;
-    }
-
-    public function fetchExercises($seanceId)
-    {
-        $query = "SELECT e.ID_exo, e.description, e.nom, e.media, cs.duree, cs.quantite 
-          FROM contenu_seance cs
-          INNER JOIN exercices e ON cs.ID_exo = e.ID_exo
-          WHERE cs.ID_seance = :seanceId
-          ORDER BY cs.rang_exo ASC";
-
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':seanceId', $seanceId, PDO::PARAM_INT);
-        $stmt->execute();
-
-        $exercises = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        return $exercises;
-    }
     public function fetchListeExercises($userId)
     {
         $query = "SELECT * FROM exercices ORDER BY nom ASC";
@@ -130,26 +97,6 @@ $seanceManager = new SeanceManager($db);
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
     $action = $_POST['action'];
     switch ($action) {
-        case 'fetchExercises':
-            if (isset($_POST['seanceId'])) {
-                $exercises = $seanceManager->fetchExercises($_POST['seanceId']);
-                echo json_encode($exercises);
-            }
-            break;
-
-        case 'otherFunction':
-            if (isset($_POST['otherData'])) {
-                $result = $seanceManager->otherFunction($_POST['otherData']);
-                echo json_encode($result);
-            }
-            break;
-        case 'fetchUserSeances':
-            if (isset($_POST['userId'])) {
-                $seances = $seanceManager->fetchUserSeances($_POST['userId']);
-                echo json_encode($seances);
-            }
-            break;
-
         case 'fetchListeExercises':
             if (isset($_POST['userId'])) {
                 $exercises = $seanceManager->fetchListeExercises($_POST['userId']);
